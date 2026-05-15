449cf70d3de4354fdf485c801b42ba548cbaae46
Nathan <an.email.com>
2026-05-15 20:17:47 +0100

    simplify http packer

--- src/transport/websocket_transport.rs
+++ src/transport/websocket_transport.rs
@@ -3,15 +3,11 @@ use async_trait::async_trait;
 use stock_trek::error::result::StockTrekResult;
 
 #[async_trait]
-pub trait WebsocketTransport<TMessage, TReply>: Transport<WebsocketPart, TMessage, TReply> {
+pub trait WebsocketTransport<TMessage, TReply>: Transport<TMessage, TReply> {
     fn send(&self, message: TMessage) -> StockTrekResult<()>;
     fn on_receive(&self, message: TReply) -> StockTrekResult<()>;
     // async fn send_and_wait_for_reply(&self, message: TMessage) -> StockTrekResult<TReply> {
     // TODO
     //     Ok(())
     // }
-}
-
-pub enum WebsocketPart {
-    BodyPart { json_path: String },
 }
