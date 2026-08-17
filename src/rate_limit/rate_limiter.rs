8272f7d50747b6a1f63e3a27dea3fe8891edad16
Nathan <an.email.com>
2026-08-17 14:23:11 +0100

    bugfix

--- src/rate_limit/rate_limiter.rs
+++ src/rate_limit/rate_limiter.rs
@@ -1,4 +1,7 @@
-use crate::rate_limit::{rate_limit_config::RateLimitConfig, rate_limiter_state::RateLimiterState};
+use crate::{
+    error::{EGError, EGResult},
+    rate_limit::{rate_limit_config::RateLimitConfig, rate_limiter_state::RateLimiterState},
+};
 use std::sync::{Arc, Mutex};
 
 #[derive(Debug, Clone)]
@@ -14,18 +17,20 @@ impl RateLimiter {
             )),
         }
     }
-    #[must_use]
-    pub fn did_acquire(&self, cost: u32) -> bool {
-        let mut limiters_guard = self.rate_limiters.lock().unwrap();
+    pub fn did_acquire(&self, cost: u32) -> EGResult<bool> {
+        let mut limiters_guard = self
+            .rate_limiters
+            .lock()
+            .map_err(|_| EGError::MutexPoisoned)?;
         for (index, limiter) in limiters_guard.iter_mut().enumerate() {
             if !limiter.did_consume(cost) {
                 for i in 0..index {
                     let limiter = &mut limiters_guard[i];
                     limiter.refund(cost);
                 }
-                return false;
+                return Ok(false);
             }
         }
-        true
+        Ok(true)
     }
 }
