8392e36c67740d890a7e209e8c22915eeb9fb7c7
Nathan <an.email.com>
2026-08-28 21:10:42 +0100

    rename test files

--- src/specs/binance/mod.rs
+++ src/specs/binance/mod.rs
@@ -3,9 +3,9 @@ mod common;
 pub(crate) mod http;
 #[cfg(feature = "iris")]
 #[cfg(test)]
-mod iris_integration;
+mod iris_integration_tests;
 #[cfg(feature = "reqwest")]
 #[cfg(test)]
-mod reqwest_integration;
+mod reqwest_integration_tests;
 #[cfg(feature = "iris")]
 pub(crate) mod websocket;
