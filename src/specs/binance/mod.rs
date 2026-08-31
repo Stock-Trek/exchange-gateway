6c0cf9ad13951453cbf57c1bc87fe7414705aeed
Nathan <an.email.com>
2026-08-31 18:07:21 +0100

    reduce visibility

--- src/specs/binance/mod.rs
+++ src/specs/binance/mod.rs
@@ -1,11 +1,13 @@
-pub(crate) mod common;
-#[cfg(feature = "reqwest")]
-pub(crate) mod http;
+mod common;
+
 #[cfg(feature = "iris")]
 #[cfg(test)]
 mod iris_integration_tests;
+#[cfg(feature = "iris")]
+pub(crate) mod websocket;
+
+#[cfg(feature = "reqwest")]
+pub(crate) mod http;
 #[cfg(feature = "reqwest")]
 #[cfg(test)]
 mod reqwest_integration_tests;
-#[cfg(feature = "iris")]
-pub(crate) mod websocket;
