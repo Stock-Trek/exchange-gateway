648668267177bd7b8d61f8cff0828fa83ae9b834
Nathan <an.email.com>
2026-09-03 13:03:46 +0100

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
-// The transports are optional features. When not all transports are enabled
-// the dormant transport machinery would otherwise be reported as dead code,
-// so only lint it once every transport is compiled in.
-#![cfg_attr(not(all(feature = "reqwest", feature = "iris")), allow(dead_code))]
-
 pub mod clock;
 pub mod connect;
 pub mod connector;
 pub mod connector_impl;
-pub mod credentials;
 pub mod error;
 pub mod functions;
 pub mod listeners;
 pub mod rate_limit;
-#[allow(dead_code)]
-pub mod sign;
 mod specs;
 mod transports;
 mod urls;
@@ -24,7 +16,6 @@ pub mod prelude {
         connect::Connect,
         connector::Connector,
         connector_impl::ConnectorImpl,
-        credentials::{api_key_credential::ApiKeyCredentials, jwt_credential::JwtCredentials},
         error::{EGError, EGResult},
         functions::ArcTryConvertValue,
         listeners::listener::ListenerTrait,
--- src/sign/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod convert_signer;
-pub mod encode;
-pub mod encrypt;
-pub mod message_signer;
-pub mod signer;
